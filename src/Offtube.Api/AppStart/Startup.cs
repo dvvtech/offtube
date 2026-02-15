9f24b613d96ffb13d5b8ce54da624c710d656e76
DVV_COMPUTER\biker <dvv153m@gmail.com>
2026-02-15 18:28:22 +0300

    add log

--- src/Offtube.Api/AppStart/Startup.cs
+++ src/Offtube.Api/AppStart/Startup.cs
@@ -6,10 +6,16 @@ namespace Offtube.Api.AppStart
     public class Startup
     {
         private WebApplicationBuilder _builder;
+        private readonly ILogger<Startup> _logger;
 
         public Startup(WebApplicationBuilder builder)
         {
             _builder = builder ?? throw new ArgumentNullException(nameof(builder));
+            var loggerFactory = _builder
+                .Services
+                .BuildServiceProvider()
+                .GetRequiredService<ILoggerFactory>();
+            _logger = loggerFactory.CreateLogger<Startup>();
         }
 
         public void Initialize()
@@ -32,6 +38,12 @@ namespace Offtube.Api.AppStart
         private void InitConfigs()
         {
             _builder.Services.Configure<AppConfig>(_builder.Configuration.GetSection(AppConfig.SectionName));
+
+            var configSection = _builder.Configuration.GetSection(AppConfig.SectionName);
+            var appConfig = configSection.Get<AppConfig>();
+
+            if(appConfig.ProxyUrl.Length > 0)
+                _logger.LogInformation($"appConfig.ProxyUrl > 0, len:{appConfig.ProxyUrl.Length}");
         }
     }
 }
